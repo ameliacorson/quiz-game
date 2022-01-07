37a3ceb20095bd0ec5370e5fc8daf5e6176f6cc6
ameliacorson <92054473+ameliacorson@users.noreply.github.com>
2022-01-07 14:08:58 -0600

    added random "correctIndex" to props

--- my-app/src/components/Game.js
+++ my-app/src/components/Game.js
@@ -8,24 +8,35 @@ export default function Game() {
         
     const [questions, setQuestions] = React.useState([])
     
+    
         React.useEffect(() => {
             fetch("https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple")
                 .then(res => res.json())
                 .then(data => setQuestions(newQuestionSet(data.results)) 
                 )
         }, [])
+
+        function scrambleAnswers(correctans, answerArray, correctIndex) {
+            answerArray.splice(correctIndex, 0, correctans)
+            console.log(answerArray)
+            return (
+                answerArray
+            )
+        }
+
+        // i need to find a way to create a "correctindex" of a random number that will save into the Question component props data. It needs to be called upon to assign the correct answer a place in the answer array, as well as compare to the players selection.
         
         function newQuestionSet(APIdata) {
            let newQuestionData = APIdata.map(quest => {
+               let correctIndex = Math.floor(Math.random()*4)
                return ({
                     id: nanoid(),
                     question: quest.question,
-                    correct_answer: quest.correct_answer,
-                    incorrect_answers: quest.incorrect_answers
+                    correctIndex: correctIndex,
+                    answers: scrambleAnswers(quest.correct_answer, quest.incorrect_answers, correctIndex),
                })
              })
 
-        console.log(Array.isArray(newQuestionData))
         return newQuestionData
         }
 
@@ -34,8 +45,8 @@ export default function Game() {
                 <Question 
                     key={quest.id}
                     question={quest.question}
-                    correct_answer={quest.correct_answer}
-                    incorrect_answers={quest.incorrect_answers}
+                    answers={quest.answers}
+                    correctIndex={quest.correctIndex}
                 />
             )
         })
--- my-app/src/components/Question.js
+++ my-app/src/components/Question.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 export default function Question(props) {
+    console.log(props.answers)
     return (
         <div className="question-container">
-        <h3> {props.question}</h3>
+        <h3> {props.question.replace(/&quot;/g,'"').replace(/&#039;/g, "'")}</h3>
         <div className="answer-container">
-            <div>{props.correct_answer}</div>
-            <div>{props.incorrect_answers[0]}</div>
-            <div>{props.incorrect_answers[1]}</div>
-            <div>{props.incorrect_answers[2]}</div>
+            <div>{props.answers[0].replace(/&quot;/g,'"').replace(/&#039;/g, "'")}</div>
+            <div>{props.answers[1].replace(/&quot;/g,'"').replace(/&#039;/g, "'")}</div>
+            <div>{props.answers[2].replace(/&quot;/g,'"').replace(/&#039;/g, "'")}</div>
+            <div>{props.answers[3].replace(/&quot;/g,'"').replace(/&#039;/g, "'")}</div>
         </div>
+        <hr></hr>
         </div>
     )
 }
